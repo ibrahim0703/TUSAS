c9098d017ebe721cee8dbfb299ad7fc84d0de368
ibrahim0703 <ibagkesen21@ku.edu.tr>
2026-03-02 13:15:02 +0300

    Update TUM-vision.py

--- TUM-vision.py
+++ TUM-vision.py
@@ -1,42 +1,15 @@
-    def get_stereo_matches(self, img_left_rect, img_right_rect):
-        # 1. Sol kameradan nokta çıkar
-        p0_left = cv2.goodFeaturesToTrack(img_left_rect, mask=None, **self.feature_params)
+        # Veri tiplerini float64 olarak zırhladık
+        self.K_left = np.array([[190.978, 0.0, 254.931], [0.0, 190.973, 256.897], [0.0, 0.0, 1.0]], dtype=np.float64)
+        self.D_left = np.array([0.003482, 0.000715, -0.002053, 0.0002029], dtype=np.float64)
         
-        if p0_left is None:
-            print("[DEBUG] KRİTİK HATA: Sol resimde TAKİP EDİLECEK HİÇBİR NOKTA BULUNAMADI!")
-            return np.array([]), np.array([])
-            
-        print(f"[DEBUG] 1. Aşama: Sol resimde {len(p0_left)} adet başlangıç noktası bulundu.")
+        self.K_right = np.array([[190.442, 0.0, 252.597], [0.0, 190.434, 254.917], [0.0, 0.0, 1.0]], dtype=np.float64)
+        self.D_right = np.array([0.003400, 0.001766, -0.002663, 0.0003299], dtype=np.float64)
 
-        # 2. Optik Akış (Lucas-Kanade)
-        p1_right, st, err = cv2.calcOpticalFlowPyrLK(img_left_rect, img_right_rect, p0_left, None, **self.lk_params)
-        
-        basarili_lk_sayisi = np.sum(st == 1)
-        print(f"[DEBUG] 2. Aşama: Optik Akış bu noktaların {basarili_lk_sayisi} tanesini sağ resimde bulduğunu iddia ediyor.")
+        self.R = np.array([
+            [ 0.99999719,  0.00160241,  0.00174676],
+            [-0.00160269,  0.9999987 ,  0.00016067],
+            [-0.0017465 , -0.00016347,  0.99999846]
+        ], dtype=np.float64)
+        self.T = np.array([-0.10093155, -0.00017163, -0.00067332], dtype=np.float64)
 
-        # 3. Epipolar Filtre
-        good_left = []
-        good_right = []
-        y_farklari = [] # Y eksenindeki sapmaları kaydedelim
-        
-        for i, (right_pt, left_pt) in enumerate(zip(p1_right, p0_left)):
-            if st[i] == 1:
-                xl, yl = left_pt.ravel()
-                xr, yr = right_pt.ravel()
-                
-                sapma = abs(yl - yr)
-                y_farklari.append(sapma)
-                
-                # FİLTREYİ GEÇİCİ OLARAK 50 PİKSELE ÇIKARDIK! (Sadece görmek için)
-                if sapma < 50.0: 
-                    good_left.append((xl, yl))
-                    good_right.append((xr, yr))
 
-        ortalama_sapma = np.mean(y_farklari) if len(y_farklari) > 0 else 0
-        max_sapma = np.max(y_farklari) if len(y_farklari) > 0 else 0
-        
-        print(f"[DEBUG] 3. Aşama: Y Eksenindeki Ortalama Sapma: {ortalama_sapma:.2f} piksel")
-        print(f"[DEBUG] 3. Aşama: Y Eksenindeki Maksimum Sapma: {max_sapma:.2f} piksel")
-        print(f"[DEBUG] SONUÇ: Eşleşen nokta sayısı: {len(good_left)}")
-
-        return np.float32(good_left), np.float32(good_right)
